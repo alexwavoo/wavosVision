b6e4c531295825d262d4bde2b66db132021d18fb
charliemcx5 <108254246+charliemcx5@users.noreply.github.com>
2024-02-03 17:46:30 +0000

    media

--- src/pages/CollectionList.js
+++ src/pages/CollectionList.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style.css';
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const query = `
-query collectionQuery {
-  collectionCollection {
-    items {
-      sys {
-        id
-      }
-      title
-      thumbnail{
-        url
+  query collectionQuery {
+    collectionCollection {
+      items {
+        sys {
+          id
+        }
+        title
+        thumbnail {
+          url
+        }
       }
     }
   }
-}
 `;
 
-export default function CollectionList() {
+const CollectionList = () => {
   const [collections, setCollections] = useState(null);
+  const [subtitlePositions, setSubtitlePositions] = useState({});
 
   useEffect(() => {
     window
@@ -42,34 +42,67 @@ export default function CollectionList() {
       });
   }, []);
 
-//   set body to overflow hidden
-useEffect(() => {
-    document.body.style.overflow = 'hidden';
+  const updateSubtitlePositions = () => {
+    if (collections) {
+      const positions = {};
+      collections.forEach((collection) => {
+        const element = document.getElementById(`collection-item-${collection.sys.id}`);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          const centerX = rect.left + rect.width / 2;
+          const centerY = rect.top + rect.height / 2;
+          positions[collection.sys.id] = { x: centerX, y: centerY };
+        }
+      });
+      setSubtitlePositions(positions);
+    }
+  };
+
+  useEffect(() => {
+    updateSubtitlePositions();
+    window.addEventListener('resize', updateSubtitlePositions);
     return () => {
-      document.body.style.overflow = 'unset';
+      window.removeEventListener('resize', updateSubtitlePositions);
     };
-  }, []);
+  }, [collections]);
 
   if (!collections) {
     return '';
   }
 
-
   return (
     <>
-        <div className='menu-wrapper'>
+      <div className='menu-wrapper'>
         <div className="menu">
-            {collections.map((collection) => (
-            <Link key={collection.sys.id} to={`/collection/${collection.sys.id}/projects`} className="collection-item">
-                <img src={collection.thumbnail.url} alt={collection.title} />
-                <p>{collection.title}</p>
+          {collections.map((collection) => (
+            <Link
+              key={collection.sys.id}
+              to={`/collection/${collection.sys.id}/projects`}
+              className="collection-item"
+              id={`collection-item-${collection.sys.id}`}
+            >
+              <img src={collection.thumbnail.url} alt={collection.title} />
+              {subtitlePositions[collection.sys.id] && (
+                <p
+                  style={{
+                    position: 'absolute',
+                    top: `${subtitlePositions[collection.sys.id].y}px`,
+                    left: `${subtitlePositions[collection.sys.id].x}px`,
+                    transform: 'translate(-50%, -50%)',
+                  }}
+                >
+                  {collection.title}
+                </p>
+              )}
             </Link>
-            ))}
+          ))}
         </div>
-        </div>
-        <Link to="/">
+      </div>
+      <Link to="/">
         <img className="logo" src="/stars.png" alt="" />
       </Link>
     </>
   );
-}
+};
+
+export default CollectionList;
