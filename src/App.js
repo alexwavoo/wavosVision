2130c8f2b48d17091d46821d6ab456624033df07
charliemcx5 <108254246+charliemcx5@users.noreply.github.com>
2024-02-03 17:02:05 +0000

    intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CollectionList from './pages/CollectionList';
 import ProjectsList from './pages/ProjectsList';
 import ProjectDetail from './pages/ProjectDetail';
 
-export default function App() {
 
+export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Check if the session is new
+    if (!sessionStorage.getItem('session')) {
+      // Set timeout to 3 seconds
+      const timeoutId = setTimeout(() => {
+        sessionStorage.setItem('session', Date.now());
+        setLoading(false);
+      }, 3000);
+
+      // Clear the timeout if the component unmounts before 3 seconds
+      return () => clearTimeout(timeoutId);
+    } else {
+      // If the session already exists, set loading to false immediately
+      setLoading(false);
+    }
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  if (loading) {
+    // Render loading message
+    return (
+      <div class="home">
+        <div class="title">WAVO'S VISION</div>
+    </div>
+    );
+  }
+
+  // If the session is new or has already been established, render the actual content
   return (
     <Router>
       <Routes>
