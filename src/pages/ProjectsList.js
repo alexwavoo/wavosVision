d377376b7deb316b21da823f15656740e6813d19
charliemcx5 <108254246+charliemcx5@users.noreply.github.com>
2024-02-06 02:41:52 +0000

    one request

--- src/pages/ProjectsList.js
+++ src/pages/ProjectsList.js
@@ -1,4 +1,3 @@
-// ProjectsList.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import '../style.css';
@@ -6,12 +5,11 @@ import { Link } from 'react-router-dom';
 
 function ProjectsList() {
   const { collectionId } = useParams();
-  const [projectIds, setProjectIds] = useState([]);
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchProjectIds = async () => {
+    const fetchData = async () => {
       try {
         const response = await fetch('https://graphql.contentful.com/content/v1/spaces/oen9jg6suzgv/', {
           method: 'POST',
@@ -21,7 +19,7 @@ function ProjectsList() {
           },
           body: JSON.stringify({
             query: `
-              query collectionEntryQuery {
+              query collectionProjectsQuery {
                 collection(id: "${collectionId}") {
                   sys {
                     id
@@ -31,10 +29,16 @@ function ProjectsList() {
                       sys {
                         id
                       }
+                      ... on Project {
+                      title
+                      thumbnail {
+                        url
+                      }
                     }
                   }
                 }
               }
+            }
             `,
           }),
         });
@@ -48,80 +52,32 @@ function ProjectsList() {
 
         const collection = data.collection;
         if (collection && collection.projectsCollection) {
-          setProjectIds(collection.projectsCollection.items.map((item) => item?.sys?.id));
+          const projectsData = collection.projectsCollection.items.map((project) => ({
+            id: project?.sys?.id,
+            title: project?.title,
+            thumbnail: project?.thumbnail?.url,
+          }));
+
+          setProjects(projectsData);
         }
       } catch (error) {
-        console.error('Error fetching project IDs:', error);
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchProjectIds();
+    fetchData();
   }, [collectionId]);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const projectPromises = projectIds.map(async (projectId) => {
-          try {
-            const response = await fetch('https://graphql.contentful.com/content/v1/spaces/oen9jg6suzgv/', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer DVunWPNQGTy0uUwexdTPIoUiShuoqOrcDGi9q8x6tXo',
-              },
-              body: JSON.stringify({
-                query: `
-                  query projectQuery {
-                    project(id: "${projectId}") {
-                      title
-                      thumbnail {
-                        url
-                      }
-                    }
-                  }
-                `,
-              }),
-            });
-
-            const { data, errors } = await response.json();
-
-            if (errors) {
-              console.error(errors);
-              return null;
-            }
-
-            return data.project;
-          } catch (projectError) {
-            console.error('Error fetching project:', projectError);
-            return null;
-          }
-        });
-
-        const resolvedProjects = await Promise.all(projectPromises);
-        const validProjects = resolvedProjects.filter((project) => project !== null);
-
-        setProjects(validProjects);
-
-
-        // apply project id to each project
-        validProjects.forEach((project, index) => {
-          project.id = projectIds[index];
-        });
-      } catch (error) {
-        console.error('Error fetching projects:', error);
-      }
-    };
-
-    if (projectIds.length > 0) {
-      fetchProjects();
-      // check if projects are in session storage
-    } 
-  }, [projectIds]);
+  if (loading) {
+    return <p>Loading...</p>; // You can replace this with a loading spinner or any loading component.
+  }
 
   if (projects.length === 0) {
     return (
       <Link to="/">
-      <img className="logo" src="/stars.png" alt="" style={{position: 'absolute', right: '0.5rem'}} />
+        <img className="logo" src="/stars.png" alt="" style={{ position: 'absolute', right: '0.5rem' }} />
       </Link>
     );
   }
@@ -132,37 +88,36 @@ function ProjectsList() {
 
   return (
     <>
-    <div className="wrapper">
+      <div className="wrapper">
         <div style={{ marginTop: '2.5rem' }}></div>
-      <div className="flex-container">
-        <div className="column-left">
-          {projectsLeft.map((project) => (
-            <Link key={project.id} to={`/collection/${collectionId}/projects/${project.id}`}>
-              <div className="grid-item" style={{marginBottom: '1.5rem'}}>
-                <img src={project?.thumbnail?.url} alt={project?.title} />
-                <div className="subtitle">{project?.title}</div>
-              </div>
-            </Link>
-          ))}
+        <div className="flex-container">
+          <div className="column-left">
+            {projectsLeft.map((project) => (
+              <Link key={project.id} to={`/collection/${collectionId}/projects/${project.id}`}>
+                <div className="grid-item" style={{ marginBottom: '1.5rem' }}>
+                  <img src={project.thumbnail} alt={project.title} />
+                  <div className="subtitle">{project.title}</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+          {projectsRight.length > 0 && (
+            <div className="column-right">
+              {projectsRight.map((project) => (
+                <Link key={project.id} to={`/collection/${collectionId}/projects/${project.id}`}>
+                  <div className="grid-item" style={{ marginBottom: '1.5rem' }}>
+                    <img src={project.thumbnail} alt={project.title} />
+                    <div className="subtitle">{project.title}</div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
-        {projectsRight.length > 0 &&
-        <div className="column-right">
-          {projectsRight.map((project) => (
-            <Link key={project.id} to={`/collection/${collectionId}/projects/${project.id}`}>
-              <div className="grid-item" style={{marginBottom: '1.5rem'}}>
-                <img src={project?.thumbnail?.url} alt={project?.title} />
-                <div className="subtitle">{project?.title}</div>
-              </div>
-            </Link>
-          ))}
-        </div>
-        }
-
       </div>
-    </div>
-    <Link to="/">
-    <img className="logo" src="/stars.png" alt="" />
-    </Link>
+      <Link to="/">
+        <img className="logo" src="/stars.png" alt="" />
+      </Link>
     </>
   );
 }
