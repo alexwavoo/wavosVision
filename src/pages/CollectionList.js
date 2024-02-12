50467cb314c79242a475b9f074d905d1f449bd17
Charles McGuire <charliemcguirex5@protonmail.com>
2024-02-12 17:26:42 +0000

    patterned

--- src/pages/CollectionList.js
+++ src/pages/CollectionList.js
@@ -132,7 +132,7 @@ if (!collections) {
           </div>
         </div>
         <Link to="/">
-        <img className={`logo ${classNames({ 'loaded': afterTransition })}`} src="/stars.png" alt="" style={{ bottom: '15px', right: '8px', position: 'absolute', transform: 'translateZ(0)' }} />
+        <img className={`logo ${classNames({ 'loaded': afterTransition })}`} src="/stars.png" alt="" style={{ display: 'none' }} />
         </Link>
       </div>
     );
