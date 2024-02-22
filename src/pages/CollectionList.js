ea7bf3b1bb5563a7ab251468f9527e0dfcc75881
Charles McGuire <charliemcguirex5@protonmail.com>
2024-02-22 18:19:47 +0000

    width

--- src/pages/CollectionList.js
+++ src/pages/CollectionList.js
@@ -140,7 +140,7 @@ const CollectionList = ({ calculatedHeight, collections, setCollections }) => {
                                 className="collection-item"
                                 id={`collection-item-${collection.sys.id}`}
                             >
-                                {/* <img src={`${collection.thumbnail.url}?h=500`} alt={collection.title} /> */}
+                                <img src={`${collection.thumbnail.url}?h=500`} style={{display: 'none', zIndex: '-10'}} alt={collection.title} />
                                 {subtitlePositions[collection.sys.id] && (
                                     <p
                                         style={{
