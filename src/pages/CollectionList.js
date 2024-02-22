be5f3669b49829626bebde25b5f38a9ea57d642c
Charles McGuire <charliemcguirex5@protonmail.com>
2024-02-22 13:33:09 +0000

    animation

--- src/pages/CollectionList.js
+++ src/pages/CollectionList.js
@@ -1,164 +1,168 @@
 import React, { useState, useEffect } from 'react';
 import '../style.css';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames';
 
+const CollectionList = ({ calculatedHeight, collections, setCollections }) => {
+    const [subtitlePositions, setSubtitlePositions] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [intro, setIntro] = useState(true);
+    const [afterTransition, setAfterTransition] = useState(false);
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+    const [counter , setCounter] = useState(0);
 
-
-const CollectionList = ({ calculatedHeight, collections, setCollections}) => {
-  const [subtitlePositions, setSubtitlePositions] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [intro, setIntro] = useState(true);
-  const [afterTransition, setAfterTransition] = useState(false);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-
-
-  useEffect(() => {
-    // Set body and id='app' to overflow hidden
-    document.body.style.overflow = 'hidden';
-    document.getElementById('app').style.overflow = 'hidden';
-  
-    // Cleanup function to reset styles when component unmounts
-    return () => {
-      document.body.style.overflow = 'unset';
-      const appElement = document.getElementById('app');
-      if (appElement) {
-        appElement.style.overflow = 'unset';
-      }
-    };
-  }, []);
-
-  const updateSubtitlePositions = () => {
-    if (collections) {
-      const positions = {};
-      collections.forEach((collection) => {
-        const element = document.getElementById(`collection-item-${collection.sys.id}`);
-        if (element) {
-          const rect = element.getBoundingClientRect();
-          const centerX = rect.left + rect.width / 2;
-          const centerY = rect.top + rect.height / 2;
-          positions[collection.sys.id] = { x: centerX, y: centerY };
-        }
-      });
-      setSubtitlePositions(positions);
-    }
-  };
-
-  useEffect(() => {
-    updateSubtitlePositions();
-    window.addEventListener('resize', updateSubtitlePositions);
-    return () => {
-      window.removeEventListener('resize', updateSubtitlePositions);
-    };
-  }, [collections, loading, intro, afterTransition, subtitlePositions]);
-
-  // download images from contentful using thumbnail urls
-    
     useEffect(() => {
-      if (collections && !loading && !intro) {
-        // setAfterTransition(true); after timeout
-        setTimeout(() => {
-          setAfterTransition(true);
-        }
-        , 2350);
-    
-  }
-}, [collections]);
+        // Set body and id='app' to overflow hidden
+        document.body.style.overflow = 'hidden';
+        document.getElementById('app').style.overflow = 'hidden';
 
-  useEffect(() => {
-
-
-    function preloadImages() {
-      return new Promise((resolve) => {
-        const images = [];
-        collections.forEach((collection) => {
-          const image = new Image();
-          image.src = `${collection.thumbnail.url}?h=500`;
-          image.onload = () => {
-            images.push(image);
-            if (images.length === collections.length) {
-              resolve();
+        // Cleanup function to reset styles when component unmounts
+        return () => {
+            document.body.style.overflow = 'unset';
+            const appElement = document.getElementById('app');
+            if (appElement) {
+                appElement.style.overflow = 'unset';
             }
-          };
-        });
-      });
+        };
+    }, []);
+
+    const updateSubtitlePositions = () => {
+        if (collections) {
+            const positions = {};
+            collections.forEach((collection) => {
+                const element = document.getElementById(`collection-item-${collection.sys.id}`);
+                if (element) {
+                    const rect = element.getBoundingClientRect();
+                    const centerX = rect.left + rect.width / 2;
+                    const centerY = rect.top + rect.height / 2;
+                    positions[collection.sys.id] = { x: centerX, y: centerY };
+                }
+            });
+            setSubtitlePositions(positions);
+        }
+    };
+
+    useEffect(() => {
+        updateSubtitlePositions();
+        window.addEventListener('resize', updateSubtitlePositions);
+        return () => {
+            window.removeEventListener('resize', updateSubtitlePositions);
+        };
+    }, [collections, loading, intro, afterTransition, subtitlePositions]);
+
+    useEffect(() => {
+        if (collections && !loading && !intro) {
+            setTimeout(() => {
+                setAfterTransition(true);
+            }, 2350);
+        }
+    }, [collections]);
+
+    useEffect(() => {
+        function preloadImages() {
+            return new Promise((resolve) => {
+                const images = [];
+                collections.forEach((collection) => {
+                    const image = new Image();
+                    image.src = `${collection.thumbnail.url}?h=500`;
+                    image.onload = () => {
+                        images.push(image);
+                        if (images.length === collections.length) {
+                            resolve();
+                        }
+                    };
+                });
+            });
+        }
+
+        if (collections) {
+            preloadImages().then(() => setImagesLoaded(true));
+        }
+
+        if (imagesLoaded) {
+            setTimeout(() => {
+                setIntro(false);
+            }, 3500);
+        }
+    }, [collections, imagesLoaded, loading, intro, afterTransition]);
+
+    useEffect(() => {
+        if (collections) {
+            const sortedCollections = collections.slice().sort((b, a) => {
+                return a.title.localeCompare(b.title);
+            });
+            setCollections(sortedCollections);
+        }
+    }, [collections]);
+
+    useEffect(() => {
+        // Create Ripple effect for each collection item
+        if (collections) {
+            collections.forEach((collection) => {
+                const element = document.getElementById(`collection-item-${collection.sys.id}`);
+                if (counter === 0 && element) {
+                      new RippleEffect({
+                        parent: element,
+                        texture: `${collection.thumbnail.url}?h=500`,
+                        intensity: 1,
+                        strength: 2,
+                        area: 4,
+                        waveSpeed: 0.008,
+                        speedIn: 1.5,
+                        speedOut: 2,
+                        easing: 'Expo.easeInOut',
+                        hover: true,
+                    });
+                    setCounter(1);
+                }
+            });
+        }
+    }, [, collections, loading, intro, afterTransition, imagesLoaded]);
+
+    if (!collections) {
+        return '';
     }
 
-    if (collections) {
-      preloadImages().then(() => setImagesLoaded(true));
+    if (intro) {
+        return (
+            <div className="home" style={{ height: `${calculatedHeight}px` }}>
+                <div className="title">WAVO'S VISION</div>
+            </div>
+        );
+    } else {
+        return (
+            <div className={`collection-wrapper ${loading ? 'invisible' : ''}`}>
+                <div className='menu-wrapper' style={{ height: `${calculatedHeight - 60}px` }}>
+                    <div className="menu">
+                        {collections.map((collection) => (
+                            <Link
+                                key={collection.sys.id}
+                                to={`/collection/${collection.sys.id}/projects`}
+                                className="collection-item"
+                                id={`collection-item-${collection.sys.id}`}
+                            >
+                                {/* <img src={`${collection.thumbnail.url}?h=500`} alt={collection.title} /> */}
+                                {subtitlePositions[collection.sys.id] && (
+                                    <p
+                                        style={{
+                                            position: 'absolute',
+                                            top: `${subtitlePositions[collection.sys.id].y}px`,
+                                            left: `${subtitlePositions[collection.sys.id].x}px`,
+                                            transform: 'translate(-50%, -50%)',
+                                        }}
+                                    >
+                                        {collection.title}
+                                    </p>
+                                )}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <Link to="/">
+                    <img className={`logo ${afterTransition ? 'loaded' : ''}`} src="/stars.png" alt="" style={{ display: 'none' }} />
+                </Link>
+            </div>
+        );
     }
-
-    if (imagesLoaded) {
-      setTimeout(() => {
-        setIntro(false);
-      }, 3500);
-    }
-  }
-  , [collections, imagesLoaded, loading, intro, afterTransition]);
-
-  useEffect(() => {
-  if (collections) {
-    // Sort collections alphabetically by title
-    const sortedCollections = collections.slice().sort((b, a) => {
-      return a.title.localeCompare(b.title);
-    });
-    setCollections(sortedCollections);
-  }
-}, [collections]);
-
-
-
-if (!collections) {
-  return '';
-  }
-  
-  if (intro) {
-      // Render loading message
-      return (
-        <div className="home" style={{ height: `${calculatedHeight}px` }}>
-          <div className="title">WAVO'S VISION</div>
-        </div>
-      );
-  } else {
-    
-    return (
-      <div className={`collection-wrapper ${classNames({ 'invisible': loading })}`}>
-        <div className='menu-wrapper' style={{ height: `${calculatedHeight - 60}px` }}>
-          <div className="menu">
-            {collections.map((collection) => (
-              <Link
-                key={collection.sys.id}
-                to={`/collection/${collection.sys.id}/projects`}
-                className="collection-item"
-                id={`collection-item-${collection.sys.id}`}
-              >
-                
-                <img src={`${collection.thumbnail.url}?h=500`} alt={collection.title} />
-                
-                {subtitlePositions[collection.sys.id] && (
-                  <p
-                    style={{
-                      position: 'absolute',
-                      top: `${subtitlePositions[collection.sys.id].y}px`,
-                      left: `${subtitlePositions[collection.sys.id].x}px`,
-                      transform: 'translate(-50%, -50%)',
-                    }}
-                  >
-                    {collection.title}
-                  </p>
-                )}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <Link to="/">
-        <img className={`logo ${classNames({ 'loaded': afterTransition })}`} src="/stars.png" alt="" style={{ display: 'none' }} />
-        </Link>
-      </div>
-    );
-  }
-  
-  
 };
 
 export default CollectionList;
